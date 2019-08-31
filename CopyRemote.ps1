7299654755d0ff1e8bdc62a89e772f5d10cd66fd
massgh <48522862+massgh@users.noreply.github.com>
2019-08-30 23:52:59 -0400

    Update CopyRemote.ps1

--- CopyRemote.ps1
+++ CopyRemote.ps1
@@ -1,8 +1,5 @@
+#run this script to download selected files and directory trees from renote sever to windows directory.
 $loc=Get-Location
-bash -c "rsync -avz --include '*/' --include={'*.xml','OUTCAR'} --exclude={'*','*rela*'} asaboor@comet.sdsc.xsede.org:/home/asaboor/work/d_core_Interface/GaAs ."
-ForEach($dir in $((Get-ChildItem -Recurse -Directory).FullName)){
-    Set-Location $dir;
-    Write-Host "$dir" -ForegroundColor Red
-    Get-VaspProjection;
-}
+bash -c "rsync -avz --include '*/' --include={'*.xml','OUTCAR'} --exclude='*' username@server:/directory/to/download /Destination/directory"
+#use above command without 'bash -c' in linux.
 Set-Location $loc
