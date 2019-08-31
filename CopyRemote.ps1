eaec1bd41bd3a4878ca6aea4c3edde76950a1889
massgh <massgh@users.noreply.github.com>
2019-08-31 00:30:36 -0400

    Update CopyRemote.ps1

--- CopyRemote.ps1
+++ CopyRemote.ps1
@@ -1,4 +1,4 @@
-#run this script to download selected files and directory trees from renote sever to windows directory.
+#run this script to download selected files and directory trees from remote sever into windows directory.
 $loc=Get-Location
 bash -c "rsync -avz --include '*/' --include={'*.xml','OUTCAR'} --exclude='*' username@server:/directory/to/download /Destination/directory"
 #use above command without 'bash -c' in linux.
