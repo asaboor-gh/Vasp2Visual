88ad272b2a43cfb29610bbc52e576a5079251a78
massgh <massgh@users.noreply.github.com>
2019-08-31 00:40:02 -0400

    Update CopyRemote.ps1
    Just added few coments.

--- CopyRemote.ps1
+++ CopyRemote.ps1
@@ -1,5 +1,5 @@
 #run this script to download selected files and directory trees from remote sever into windows directory.
-$loc=Get-Location
+$loc=Get-Location #location of directory where to download
 bash -c "rsync -avz --include '*/' --include={'*.xml','OUTCAR'} --exclude='*' username@server:/directory/to/download /Destination/directory"
 #use above command without 'bash -c' in linux.
 Set-Location $loc  #go back at top of directory tree
