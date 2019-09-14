ef4ed8ce57daaca1871f7f54d0eeed12dd1c5e4b
Abdul Saboor <48522862+massgh@users.noreply.github.com>
2019-09-14 17:10:02 -0400

    Add files via upload

---
+++ Vasp2Visual/KPathGenerator.ps1
@@ -0,0 +1,26 @@
+﻿Remove-Item .\KPath.txt -Force -ErrorAction Ignore
+[int]$N=Read-Host "Enter number of High Symmetry KPOINTS (L,M,X,..). [Integer]"
+[int]$steps=Read-Host "Enter number of kpoints in a single High Symmetry Interval (L,M). [Integer]"
+$steps=$($steps-1); #
+[array]$kpt1=(Read-Host "Type KPOINT # 1. [like 0,0,0 1D Array]").Split(",")
+for($i=1;$i -lt $N;$i++){
+[array]$kpt2=(Read-Host "Type KPOINT # $($i+1). [like 0,0,0 1D Array]").Split(",")
+$values=($kpt1,$kpt2)
+$dx=($values[1][0]-$values[0][0])/$steps
+$dy=($values[1][1]-$values[0][1])/$steps
+$dz=($values[1][2]-$values[0][2])/$steps
+$point= "$($values[0][0])    $($values[0][1])     $($values[0][2])     0"
+$point|Add-Content .\KPath.txt
+$sum=$values[0][0],$values[0][1],$values[0][2]
+for($x=1; $x -le ($steps-1); $x++){
+$sum[0]=[Math]::Round($dx+$sum[0],5); $sum[0]="{0:n4}" -f  $($sum[0])
+$sum[1]=[Math]::Round($dy+$sum[1],5);  $sum[1]="{0:n4}" -f  $($sum[1])
+$sum[2]=[Math]::Round($dz+$sum[2],5); $sum[2]="{0:n4}" -f $($sum[2])
+$point= "   $($sum[0])    $($sum[1])     $($sum[2])     0"
+$point|Add-Content .\KPath.txt
+} $kpt1=$kpt2 #switch
+$point= "$($values[1][0])    $($values[1][1])     $($values[1][2])     0"
+$point|Add-Content .\KPath.txt
+}
+Write-Host "Opening the file KPath.txt ..." -ForegroundColor DarkCyan
+Start-Process .\KPath.txt
---
+++ Vasp2Visual/vasprunKpts.ps1
@@ -0,0 +1,29 @@
+﻿#$xml= New-Object Xml  #To load files bigger than 0.5GB.
+#$xml.Load((Convert-Path .\vasprun.xml))
+#$xml = [xml](get-content .\vasprun.xml)
+#$NKPT=$xml.modeling.calculation.eigenvalues.array.set.set.set.Length
+[float]$sum=0.00001;# To make Round
+$loc=Get-Location
+#Add-Content -Path  .\Kpts.txt -Value "$sum"
+$swk = New-Object System.IO.StreamWriter "$($loc)\Kpts.txt"
+$Writers+=$swk;
+$swk.WriteLine("Reference")
+#Write-Host "Type 0 to Include all KPOINTS, otherwise 
+#Enter Number of KPOINTS to EXCLUDE: " -ForegroundColor Red -NoNewline
+#[int]$ibzkpt=Read-Host  #Reads NKPTS to exclude
+$old=$xml.modeling.kpoints.varray.v[0].trim().split()|Where-Object{$_}
+for ($a=$ibzkpt; $a -le ($NKPT-1); $a++){
+$com=$xml.modeling.kpoints.varray.v[$a].trim().split()|Where-Object{$_}
+$cr1=[Math]::pow(($com[0]-$old[0]),2); $cr2=[Math]::pow(($com[1]-$old[1]),2); $cr3=[Math]::pow(($com[2]-$old[2]),2)
+$val=[Math]::Round([Math]::Sqrt($cr1+$cr2+$cr3),6)
+if($a -eq $ibzkpt){$ref=$val} #set reference to zero
+$old=$com; 
+$sum+=$val;
+$value= "{0:n4}" -f $([Math]::Round($($sum-$ref),4)) #formatting decimal places
+#Add-Content -Path  .\Kpts.txt -Value "$sum"
+$swk.WriteLine("$value")
+}
+$swk.Close()
+$content = Get-Content -Path .\Kpts.txt   #Replacing fist line by a comment.
+$content[0]="        [k_i-k_0]"
+$content | Set-Content -Path .\Kpts.txt
