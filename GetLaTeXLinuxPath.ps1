0186fee854b293c3b5e179a4511703033c60c2ed
Abdul Saboor <48522862+massgh@users.noreply.github.com>
2019-09-15 11:37:34 -0400

    Delete GetLaTeXLinuxPath.ps1

--- GetLaTeXLinuxPath.ps1
+++
@@ -1,7 +0,0 @@
-#Get Linux and LaTeX path from current working directory in Windows Powershell.﻿
-$winpath=Get-Location
-$linuxPath =(($winpath -replace "\\","/") -replace ":","").ToLower().Trim("/")
-$latexPath = ($winpath -replace "\\","/").Trim("/")
-Write-Host "Current Windows directory: $winpath" -ForegroundColor DarkGreen
-Write-Host "LaTeX Path:  $latexPath" -ForegroundColor Magenta
-Write-Host "Linux Path:  $linuxPath" -ForegroundColor Cyan
