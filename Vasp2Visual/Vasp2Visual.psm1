305435ca7d686230165ae5fad55c6878dc336390
Abdul Saboor <48522862+massgh@users.noreply.github.com>
2019-09-24 11:41:49 -0400

    Add files via upload

--- Vasp2Visual/Vasp2Visual.psm1
+++ Vasp2Visual/Vasp2Visual.psm1
@@ -32,13 +32,14 @@ Write-Host "All opened StreamWriters are now closed." -ForegroundColor Green
 }
 Function Get-PlotArguments{ #Creates an ordered hashtable to use in plot arguments
 [ordered]@{tickIndices="[0,25,50,75,100,-1]"; ticklabels="['L',r'$\Gamma$','X','W','K',r'$\Gamma$']";
-E_Limit="[10,-15]"; DOS_Limit="[0.0,1.2]"; WidthToColumnRatio=1;
+E_Limit="[10,-15]"; DOS_Limit="[0.0,1.2]"; textLocation="[0.05,0.9]"
 ProLabels="['Ga','s','p','d']"; ProIndices="[(range(0,1,1)),(0,),(1,2,3,),(4,5,6,7,8,)]";}
 }
 Function Get-Plot{ #Plots of different types
 [CmdletBinding()]
-Param([Parameter()][switch]$ProjectedBandsDOS, 
-#[Parameter()][switch]$ProjectedBands, [Parameter()][switch]$ProjectedDOS,
+Param([Parameter()][switch]$HalfColumnWidth,
+[Parameter()][switch]$ProjectedBandsDOS, 
+[Parameter()][switch]$ProjectedBands, #[Parameter()][switch]$ProjectedDOS,
  [Parameter()][switch]$BandsDOS,#[Parameter()][switch]$DOS, [Parameter()][switch]$Bands,
 [hashtable]$PlotArguments)  #Get Hashtable from function Get-PlotArguments
 #making a plot file in order
@@ -50,11 +51,14 @@ $consoleInput=@"
 $variablesList
 "@
 . $PSScriptRoot\BDPlotFile.ps1
+if($HalfColumnWidth.IsPresent){$WidthToColumnRatio=0.5}Else{$WidthToColumnRatio=1.0}
 if($ProjectedBandsDOS.IsPresent){$FileInput=$FileString}
 if($BandsDOS.IsPresent){$FileInput=$SimpleFileString}
+if($ProjectedBands.IsPresent){$FileInput=$ProjectedBandsFileString}
 $pythonFileContent=@"
 #=================Input Variables=====================
 $($consoleInput)
+WidthToColumnRatio=$($WidthToColumnRatio);
 $($FileInput)
 "@
 $pythonFileContent|Set-Content .\Plot.py
