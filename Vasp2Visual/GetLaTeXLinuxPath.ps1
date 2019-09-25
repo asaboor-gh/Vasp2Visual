a014c5285b86b20bacdee560ab12dc818f3e3c58
Abdul Saboor <48522862+massgh@users.noreply.github.com>
2019-09-25 14:51:07 -0400

    Delete GetLaTeXLinuxPath.ps1

--- Vasp2Visual/GetLaTeXLinuxPath.ps1
+++
@@ -1,8 +0,0 @@
-﻿$winpath=Get-Location
-$drive=$winpath.Drive.Name.ToLower(); $path=Split-Path $winpath -NoQualifier
-$linuxPath = -Join('/mnt/',-Join($drive,(($path -replace "\\","/") -replace ":","")))
-$latexPath = ($winpath -replace "\\","/").Trim("/")
-Write-Host "Current directory is copied to Clipboard as: " -ForegroundColor Green -NoNewline
-Write-Host "Linux Path:  $linuxPath" -ForegroundColor Cyan
-Write-Host "LaTeX Path:  $latexPath " -ForegroundColor Magenta 
-Set-Clipboard  "$linuxPath";
